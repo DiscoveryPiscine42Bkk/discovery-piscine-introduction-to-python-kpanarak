970780e448637fb878b7376052d30e59ce13abe0
kpanarak <kanokwan.m4911@gmail.com>
2025-06-17 08:15:50 +0000

    add mult.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first nunber:"))
+num2 = int(input("Enter the second nunber:"))
+print(f"{num1} x {num2} = {num1 * num2}")
+if num1 * num2 > 0:
+    print("The result is positive.")
+elif num1 * num2 < 0:
+    print("The result is negative.")
