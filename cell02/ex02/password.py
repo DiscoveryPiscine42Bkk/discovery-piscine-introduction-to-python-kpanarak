840b387ff978d8da382c8a64cc1bc2ded00b4305
kpanarak <kanokwan.m4911@gmail.com>
2025-06-18 08:50:29 +0000

    add

--- cell02/ex02/password.py
+++ cell02/ex02/password.py
@@ -2,4 +2,5 @@ password = "Python is awesome"
 prompt = input()
 if prompt == password:
     print("ACCESS GRANTED")
+else:
     print("ACCESS DENIED")
--- cell05/ex07/downcase_it.py
+++ cell05/ex07/downcase_it.py
@@ -1,3 +1,7 @@
-def downcase_it(text):
-    return text.lower()
-print(downcase_it("RTFM (Read the F-ing manual)"))
+import sys
+def main():
+    if len(sys.argv) == 2:
+          print(sys.argv[1].lower())
+    else:
+       print("none")
+main()
