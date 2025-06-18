d9f092cfae79f035106fa38924254afbc49711bc
kpanarak <kanokwan.m4911@gmail.com>
2025-06-18 09:38:07 +0000

    add ex10

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,12 @@
+import sys
+def main():
+    if len(sys.argv) != 2:
+       print("none")
+       return
+    param = sys.argv[1]
+    answer = input("What was the parameter?")
+    if answer == param:
+       print("Good job!")
+    else:
+       print("Nope, sorry..")
+main()
